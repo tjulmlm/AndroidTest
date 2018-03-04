6725592f3b0d2751fc079b1ec780491496b11cbd
mulin <linmu@bytedance.com>
2018-03-04 15:36:17 +0800

    branch2 test

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,6 +8,6 @@ JNICALL
 Java_com_example_mulin_androidtest_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++ test branch2";
     return env->NewStringUTF(hello.c_str());
 }
