40de464e8d78bdb15fd09250aa8f3f3f9d881d88
linmu <linmu@bytedance.com>
2018-12-05 15:34:29 +0800

    a test

--- app/src/main/java/com/example/mulin/androidtest/MainActivity.java
+++ app/src/main/java/com/example/mulin/androidtest/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int master = 1;
+        int alpha = 22;
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
