62a79f3e7bf502861ccdb4cf60785443be007685
linmu <linmu@bytedance.com>
2018-12-05 12:05:03 +0800

    test b=2

--- app/src/main/java/com/example/mulin/androidtest/MainActivity.java
+++ app/src/main/java/com/example/mulin/androidtest/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String chrry = "pick";
+        int b= 2;
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
